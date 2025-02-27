69660ece22a45e04f50af485ba3e60bc9f7dda77
Belete <belete143@gmail.com>
2025-02-27 04:12:26 -0500

    fetching title

--- scripts.js
+++ scripts.js
@@ -8,8 +8,13 @@ async function getStarWars() {
     try {     
       const response = await fetch('https://swapi.dev/api/films/');
       const data = await response.json();
-      console.log(data);
+      const movies = data.results;
 
+      movies.forEach(movie => {
+        var table = document.createElement('thead');
+        table.innerHTML = `<tr><td>${movie.title} </td> </tr>`
+        document.getElementById('ListItem').appendChild(table);
+      });
     } catch (error) {
       console.error(error);
     }
