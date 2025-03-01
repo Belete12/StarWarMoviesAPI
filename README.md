4e369e4b9c72e338f9b20a17908a2286708432ba
Belete <belete143@gmail.com>
2025-03-01 16:21:30 -0500

    wrapping up all the missed

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # StarWarMoviesAPI
 *The starwar APi is going to fetch the film list and charcter details
 *The Starwar API has three different pages
-- Home page is the welcome page that routes to films and characters page
-- The Character page is the details of the star war film characters like :
-   Actor, 
-   Fims
-- The Index page is for films detail like: 
-   Title , 
-   Episod ID
+* Home page is the welcome page that routes to 
+   - Films 
+   - Characters page
+   -  Also, the Picture is link to all the star war movies poster
+* The Character page is the details of the star war film characters like :
+   - Actor
+   - Fims
+* The Index page is for films detail like: 
+   - Title  
+   - Episod ID
 
 
--- charcter.js
+++ charcter.js
@@ -5,16 +5,53 @@ async function getCharacterDetails() {
       const movies = data.results;
 
       movies.forEach(movie => {
+
         var table = document.createElement('thead');
+
         const mylist = movie.films;
+
         const type = mylist.join(",\n");
+
         table.innerHTML = `<tr><td>${movie.name} </td> <td>${movie.gender} </td> <td>${movie.homeworld} </td> <td>${type} </td> </tr>`
+
         document.getElementById('movieList').appendChild(table);
-      });
+
+      }); //endfor loop
     } catch (error) {
       console.error(error);
     }
   } // end function
-  
 
   getCharacterDetails();
+
+  //Filiter actors Name
+
+  function displayText()  {
+    const input = document.getElementById("SearchName");
+
+    const table = document.getElementById("movieList");
+
+    const filterData = input.value.toUpperCase();
+
+    const actorName = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < actorName.length; i++) {
+
+      const tableData = actorName[i].getElementsByTagName("td")[0];
+
+      if (tableData) {
+        const data= tableData.textContent || tableData.innerText;
+
+        if(data.toUpperCase().indexOf(filterData) > -1){
+
+          actorName[i].style.display = "";
+        }
+        else{
+          actorName[i].style.display = "none";
+      }    
+      } //end if
+    }//end forloop
+    } //end Func
+
+
+
